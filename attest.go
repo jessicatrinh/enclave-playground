53d39b34beddbf7a0fa0a86c236b77fa0ac2376f
Jessica Trinh <jessica.trinh@blockfi.com>
2022-04-25 16:57:57 -0700

    Pull subfunctions from main and save errors to file

--- attest.go
+++ attest.go
@@ -17,6 +17,9 @@ import (
 	"github.com/jessicatrinh/nsm/request"
 )
 
+var path = "errors.txt"
+
+
 // generateBigPrime reads entropy from Nitro Secure Module (https://github.com/hf/nsm)
 func generateBigPrime() (*big.Int, error) {
 	sess, err := nsm.OpenDefaultSession()
@@ -66,29 +69,113 @@ func StreamToByte(stream io.Reader) []byte {
 	return buf.Bytes()
 }
 
-func main() {
+// Handling text files (https://www.golangprograms.com/golang-read-write-create-and-delete-text-file.html)
+
+func isError(err error) bool {
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	return (err != nil)
+}
+
+// createFile creates (if it doesn't already exist) and opens a file
+func createFile() *os.File {
+	// check if file exists
+	var _, err = os.Stat(path)
+
+	// delete file
+	//if os.IsExist(err) {
+	//	var err = os.Remove(path)
+	//	if isError(err) {
+	//		return
+	//	}
+	//}
+
+	// create file if not exists
+	if os.IsNotExist(err) {
+		var file, err = os.Create(path)
+		if isError(err) {
+			return nil
+		}
+		defer file.Close()
+	}
+
+	// Open file using READ & WRITE permission.
+	var file2, err2 = os.OpenFile(path, os.O_RDWR, 0644)
+	if isError(err2) {
+		return nil
+	}
+	return file2
+}
+
+func writeFile(file *os.File, myErr error) {
+	// Write some text line-by-line to file
+	_, err := file.WriteString(myErr.Error() + "\n")
+	if isError(err) {
+		return
+	}
+	// Save file changes
+	err = file.Sync()
+	if isError(err) {
+		return
+	}
+}
+
+// Pull out into sub-functions:
+
+// getRand returns random number, or writes the error to a file
+func getRand(file *os.File) *bytes.Reader {
 	// Use NSM's random number generator
 	randomInt, err := generateBigPrime()
+	// Save to file any error returned by generateBigPrime from nsm interface library
 	if err != nil {
+		writeFile(file, err)
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
-	random := bytes.NewReader(randomInt.Bytes())
-	// random := rand.Reader // Uncomment for TEST
+	return bytes.NewReader(randomInt.Bytes())
+	// return rand.Reader // Uncomment for TEST
+}
+
+// getXpub generates a keypair and return its public key as a byte array, or writes the
+// error to a file
+func getXpub(file *os.File, random *bytes.Reader) []byte {
 	// Generate a keypair with ECC
 	curve := elliptic.P256()
 	xprv, err := ecdsa.GenerateKey(curve, random)
+	// Save to file any error returned by GenerateKey from ecdsa library
 	if err != nil {
+		writeFile(file, err)
 		fmt.Fprintf(os.Stderr, "error: %v\n", err)
 		os.Exit(1)
 	}
 	xpub := xprv.Public()
-	userData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
-	// Retrieve attestation document
-	att, err := attest(StreamToByte(random), userData, xpub.([]byte))
+	return xpub.([]byte)
+}
+
+
+// getAttest retrieves the attestation document, or writes the error to a file
+func getDoc(file *os.File, random *bytes.Reader, userData []byte, xpubBytes []byte) {
+	att, err := attest(StreamToByte(random), userData, xpubBytes)
+	// Save to file any error returned by attest from nsm interface library
+	if err != nil {
+		writeFile(file, err)
+	}
 	fmt.Printf("attestation %v %v\n", base64.StdEncoding.EncodeToString(att), err)
+}
+
+// MAIN:
+
+func main() {
+	file := createFile()
+	random := getRand(file)
+	xpubBytes := getXpub(file, random)
+	userData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+	getDoc(file, random, userData, xpubBytes)
+	defer file.Close()
 	for {
-		time.Sleep(1 * time.Second)
 		fmt.Println("Running attest")
+		time.Sleep(3 * time.Second)
 	}
 }
