8f68dc453714f47c7b4a37cd10b14682c76c66a3
Jessica Trinh <jessica.trinh@blockfi.com>
2022-04-26 13:05:00 -0700

    Use sample xpub

--- nsm.go
+++ nsm.go
@@ -25,8 +25,9 @@ func main() {
 		}
 
 		fmt.Println("Creating keypair")
-		xpub, err := getXpub()
-		logIfError(err)
+		//xpub, err := getXpub()
+		//logIfError(err)
+		xpub := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11} 	// sample xpub
 
 		userData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11} 	// sample userData
 
