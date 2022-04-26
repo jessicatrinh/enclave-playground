e9fdccc65c9a0d46deb2cb26347d31eccc14c0b8
Jessica Trinh <jessica.trinh@blockfi.com>
2022-04-26 13:16:28 -0700

    Uncomment xpub part

--- nsm.go
+++ nsm.go
@@ -1,18 +1,18 @@
 package main
 
 import (
-	"crypto/rand"
+	"bytes"
 	"crypto/ecdsa"
 	"crypto/elliptic"
+	"crypto/rand"
 	"encoding/base64"
 	"errors"
 	"fmt"
+	"github.com/jessicatrinh/nsm"
 	"github.com/jessicatrinh/nsm/request"
+	"io"
 	"math/big"
 	"time"
-	"io"
-	"bytes"
-	"github.com/jessicatrinh/nsm"
 )
 
 func main() {
@@ -29,7 +29,7 @@ func main() {
 		logIfError(err)
 		// xpub = []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11} 	// sample xpub
 
-		userData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11} 	// sample userData
+		userData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11} // sample userData
 
 		fmt.Println("Generating attestation doc")
 		doc, err := attest([]byte{0, 1, 2, 3, 4, 5, 6, 7}, userData, xpub)
@@ -58,7 +58,6 @@ func StreamToByte(stream io.Reader) []byte {
 	return buf.Bytes()
 }
 
-
 // getXpub generates a keypair and return its public key as a byte array
 func getXpub() ([]byte, error) {
 	// get sess
@@ -72,12 +71,14 @@ func getXpub() ([]byte, error) {
 	curve := elliptic.P256()
 	xprv, err := ecdsa.GenerateKey(curve, sess)
 	fmt.Println("xprv", xprv)
+	if err != nil {
+		return nil, err
+	}
+	xpub := xprv.Public()
+	fmt.Println("xpub", xpub)
+
 	return nil, nil
 
-	//if err != nil {
-	//	return nil, err
-	//}
-	//xpub := xprv.Public()
 	//return xpub.([]byte), nil
 }
 
