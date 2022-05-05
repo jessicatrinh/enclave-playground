3c624212f1db52b83b983c9006c98e35ebf63624
Jessica Trinh <jessica.trinh@blockfi.com>
2022-05-05 16:54:04 -0700

    Temp print nonce used in doc

--- nsm.go
+++ nsm.go
@@ -12,5 +12,6 @@ func main() {
 		fmt.Errorf("cannot create nonce: %v", err)
 	}
 	userData := []byte{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11}
+	fmt.Println("nonce", nonce)
 	attestation.Attest(nonce, userData)
 }
